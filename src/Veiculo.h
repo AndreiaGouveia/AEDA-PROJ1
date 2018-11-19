c9f94f70315ae38282d73de5e41f45f75e720e39
Cajo Albuquerque <carlosjo.da@gmail.com>
2018-11-19 10:01:53 +0000

    Major changes to class Veiculo
    Not complete changes, but messed up with most of the atributes and added new methods

--- src/Veiculo.h
+++ src/Veiculo.h
@@ -3,40 +3,44 @@
 
 using namespace std;
 
-class Veiculo
-{
+class Veiculo {
 private:
-	    static unsigned int numVeiculos;
-	    const unsigned int idV;
-		const string matricula;
-		const int capacDeposito;
-		//const string tipoComb; -> é desnecessario, por isso vamos poupar uma variavel
-		float precoComb;
-		float consumoPerc;
-		//int numero; -> a ideia de numVeiculos é ser o idV de cada veiculo adicionado
-		vector <unsigned int> zonasAtravessadas;
-
+	static unsigned int numVeiculos;
+	const unsigned int idV;
+	const string matricula;
+	const int capacDeposito;
+	//const string tipoComb; -> é desnecessario, por isso vamos poupar uma variavel
+	float precoComb;
+	float consumo100km;
+	//int numero; -> a ideia de numVeiculos é ser o idV de cada veiculo adicionado
 public:
-		Veiculo(const string &matricula,const int &capacDeposito,const float &precoComb, const float &consumoPerc);
-		float calcGasto();
-		void adicionarZona(unsigned int zona);
-		void removerZona(unsigned int zona);
+	Veiculo(const string &matricula, const int &capacDeposito, const float &precoComb, const float &consumoPerc);
+	virtual ~Veiculo() {
+	}
+	virtual float calcGasto() const = 0;
+	unsigned int getId() const;
+	string getMatricula() const;
 };
 
-class	TransporteEscolar : public Veiculo
-{
+class Escolar: public Veiculo {
 private:
 	unsigned int lotacao;
 	unsigned int lugaresLivres;
+	vector<unsigned int> zonasAtravessadas;
 public:
-	TransporteEscolar(const string matricula,const int capacDeposito,float precoComb,float consumoPerc,unsigned int lotacao,unsigned int lugaresLivres);
-
+	Escolar(const string matricula, const int capacDeposito, float precoComb, float consumoPerc, unsigned int lotacao, unsigned int lugaresLivres);
+	void adicionarZona(unsigned int zona);
+	void removerZona(unsigned int zona);
+	float calcGasto() const;
+	friend ostream& operator <<(ostream& out, const Escolar& veic);
 };
-class	TransporteActividadeRecreativa:public Veiculo
-{
+class Recreativo: public Veiculo {
 private:
 	bool alugado;
 	const unsigned int capacidade;
+	float kmsPercorridos;
 public:
-	TransporteActividadeRecreativa(const string matricula,const int capacDeposito,float precoComb,float consumoPerc,bool alugado,const unsigned int capacidade);
+	Recreativo(const string matricula, const int capacDeposito, float precoComb, float consumoPerc, bool alugado, const unsigned int capacidade);
+	float calcGasto() const;
+	friend ostream& operator <<(ostream& out, const Recreativo& veic);
 };
