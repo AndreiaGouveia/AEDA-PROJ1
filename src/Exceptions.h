a3888f8bf1ac89541ffd127f6df001c1b254c45c
Sofia Lajes <up201704066@fe.up.pt>
2018-11-24 00:52:06 +0000

    Fixed Exceptions

--- src/Exceptions.h
+++ src/Exceptions.h
@@ -10,7 +10,7 @@ class ZonaInexistente
 {
 	ostringstream msg;
 public:
-	ZonaInexistente(const unsigned int &z) { msg << "A zona " << z << " não existe."; }
+	ZonaInexistente(const unsigned int &z) { msg << "A zona " << z << " nao existe."; }
 	string getMsg() { return msg.str(); }
 };
 
@@ -18,7 +18,7 @@ class ZonaJaExiste
 {
 	ostringstream msg;
 public:
-	ZonaJaExiste(const unsigned int &z) { msg << "A zona " << z << " já existe."; }
+	ZonaJaExiste(const unsigned int &z) { msg << "A zona " << z << " ja existe."; }
 	string getMsg() { return msg.str(); }
 };
 
@@ -27,7 +27,7 @@ class VeiculoNaoExistente
 private:
 	string msg;
 public:
-	VeiculoNaoExistente() { msg = "Veiculo não existente."; }
+	VeiculoNaoExistente() { msg = "Veiculo nao existente."; }
 	string getMsg() { return msg; }
 };
 
@@ -36,7 +36,7 @@ class VeiculoJaExiste
 private:
 	string msg;
 public:
-	VeiculoJaExiste(const string &matricula) { msg = "Veiculo de matricula " + matricula + " já se encontra registado"; }
+	VeiculoJaExiste(const string &matricula) { msg = "Veiculo de matricula " + matricula + " ja se encontra registado"; }
 	string getMsg() { return msg; }
 };
 
@@ -45,7 +45,7 @@ class VeiculosInsuficientes
 private:
 	string msg;
 public:
-	VeiculosInsuficientes() { msg = "Não há veículos suficientes"; }
+	VeiculosInsuficientes() { msg = "Nao ha veiculos suficientes"; }
 	string getMsg() { return msg; }
 };
 
@@ -54,7 +54,7 @@ class VeiculoNaoEscolar
 private:
 	string msg;
 public:
-	VeiculoNaoEscolar() { msg = "O veículo não é escolar!"; }
+	VeiculoNaoEscolar() { msg = "O veiculo nao e escolar!"; }
 	string getMsg() { return msg; }
 };
 
@@ -63,7 +63,7 @@ class VeiculoNaoRecreativo
 private:
 	string msg;
 public:
-	VeiculoNaoRecreativo() { msg = "O veículo não é recreativo!"; }
+	VeiculoNaoRecreativo() { msg = "O veiculo nao e recreativo!"; }
 	string getMsg() { return msg; }
 };
 
@@ -72,7 +72,7 @@ class UtenteNaoExistente
 private:
 	string msg;
 public:
-	UtenteNaoExistente() { msg = "Veiculo não existente."; }
+	UtenteNaoExistente() { msg = "Veiculo nao existente."; }
 	string getMsg() { return msg; }
 };
 
@@ -81,7 +81,7 @@ class UtenteJaExiste
 private:
 	string msg;
 public:
-	UtenteJaExiste(const string &numUtente) { msg = "O utente de número " + numUtente + " já se encontra registado"; }
+	UtenteJaExiste(const string &numUtente) { msg = "O utente de numero " + numUtente + " ja se encontra registado"; }
 	string getMsg() { return msg; }
 };
 
@@ -90,7 +90,7 @@ class PrecoNaoDefinido
 private:
 	string msg;
 public:
-	PrecoNaoDefinido() { msg = "O preço por pessoa não está definido ainda."; }
+	PrecoNaoDefinido() { msg = "O preço por pessoa nao esta definido ainda."; }
 	string getMsg() { return msg; }
 };
 
