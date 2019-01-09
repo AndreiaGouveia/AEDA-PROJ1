05d05a04584f5b6773bfb90c04209468e396183f
Sofia Lajes <up201704066@fe.up.pt>
2019-01-09 22:52:59 +0000

    Small reading improvements

--- src/EmpresaTransportes.cpp
+++ src/EmpresaTransportes.cpp
@@ -1612,9 +1612,9 @@ string Empresa::getEscolas() {
 		while (!itr.isAtEnd())
 		{
 			if (itr.retrieve().getZona() == zona) {
-				s << itr.retrieve().getCodigo() << '\t'
-						<< itr.retrieve().getNome() << '\t'
-						<< itr.retrieve().getDiretorInfo().first << '\n';
+				s << "Codigo: " << itr.retrieve().getCodigo() << "     "
+						<< "Nome: " << itr.retrieve().getNome() << "     "
+						<< "Diretor: " << itr.retrieve().getDiretorInfo().first << '\n';
 			}
 
 			itr.advance();
@@ -1630,6 +1630,8 @@ string Empresa::getEscolasZona(unsigned zona) {
 	BSTItrIn<Escola> itr(escolas);
 
 	ostringstream s;
+
+	s << "=== Escolas na zona " << zona << " ===" << endl;
 
 	while (!itr.isAtEnd()) {
 		if (itr.retrieve().getZona() == zona) {
